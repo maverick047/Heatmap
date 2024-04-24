18d4164cd63bf7d23dddab0ef6bf13e421646fef
Rahul <127756378+maverick047@users.noreply.github.com>
2024-04-25 00:10:06 +0530

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -2,6 +2,6 @@ import seaborn as sns
 import pandas as pd     
 import matplotlib.pyplot as plt
 
-iris = pd.read_csv("C:\\Heatmao\\iris.csv")
+iris = pd.read_csv("file-path")
 sns.heatmap(iris.corr(numeric_only=True), annot=True, cmap="viridis")
 plt.show()
